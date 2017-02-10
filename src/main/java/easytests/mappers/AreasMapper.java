415f48ee0a2cabcaed8588cf1bc17be4b3940961
FortyWays <zedront307@gmail.com>
2017-02-11 02:32:43 +0300

    Insert, delete and update for areas

--- src/main/java/easytests/mappers/AreasMapper.java
+++ src/main/java/easytests/mappers/AreasMapper.java
@@ -1,44 +1,43 @@
 package easytests.mappers;
 
-
 import easytests.entities.Area;
 import easytests.entities.Dweller;
+import java.util.List;
 import org.apache.ibatis.annotations.*;
 
-import java.util.List;
-
-
 /**
- * Created by fortyways on 06.02.17.
+ * @author fortyways  06.02.17.
  */
 @Mapper
 public interface AreasMapper {
 
-    @Select("SELECT * FROM areas ")///where id=#{id}
+    @Select("SELECT * FROM areas")
     @Results(value =
             {
-                    @Result(property = "id", column = "id"),
+                    //@Result(property = "id", column = "id"),
                     @Result(property = "name", column = "area_name"),
                     @Result(property = "xCoord", column = "x_coord"),
                     @Result(property = "yCoord", column = "y_coord"),
                     @Result(property = "width", column = "width"),
                     @Result(property = "length", column = "height"),
-                    @Result(property = "habitants", javaType = List.class, column = "id", many = @Many(select = "FindDwellers"))
+                    @Result(property = "habitants", javaType = List.class,
+                            column = "id", many = @Many(select = "findDwellers"))
             })
-    List<Area> FindAllAreas();
+    List<Area> findAllAreas();
 
     @Select("SELECT * FROM areas where id=#{id}")
     @Results(value =
             {
-                    @Result(property = "id", column = "id"),
+                    //@Result(property = "id", column = "id"),
                     @Result(property = "name", column = "area_name"),
                     @Result(property = "xCoord", column = "x_coord"),
                     @Result(property = "yCoord", column = "y_coord"),
                     @Result(property = "width", column = "width"),
                     @Result(property = "length", column = "height"),
-                    @Result(property = "habitants", javaType = List.class, column = "id", many = @Many(select = "FindDwellers"))
+                    @Result(property = "habitants", javaType = List.class,
+                            column = "id", many = @Many(select = "findDwellers"))
             })
-    Area FindAreaById(int id);
+    Area findAreaById(int id);
 
     @Select("select * from dwellers where area_id=#{id}")
     @Results(value =
@@ -46,6 +45,16 @@ public interface AreasMapper {
                     @Result(property = "lastName", column = "last_name"),
                     @Result(property = "surname", column = "surname")
                     })
-    List<Dweller> FindDwellers(int id);
+    List<Dweller> findDwellers(int id);
 
+    @Insert("INSERT INTO areas(area_name, x_coord, y_coord, width, height) VALUES(#{name}, #{xCoord}, #{yCoord}," +
+            " #{width}, #{length})")
+    @Options(useGeneratedKeys = true, keyColumn = "id")
+    void create(Area area);
+
+    @Delete("DELETE FROM areas WHERE id=#{id}")
+    void delete(Area area);
+
+    @Update("UPDATE areas SET area_name=#{name}, x_coord=#{xCoord}, y_coord=#{yCoord}, width=#{width}, height=#{length} WHERE id=#{id}")
+    void update(Area area);
 }
